0505f54d512a99cc3871768100ddcc2037fe10ef
leadgfl13 <leadgfl13@gmail.com>
2024-06-20 22:27:32 -0400

    Run prettier through files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # basic-template
-A basic templace to act as a launching pad for future projects.  Uses npm and webpack to bundle everything together.  
+
+A basic templace to act as a launching pad for future projects. Uses npm and webpack to bundle everything together.
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,38 @@
-const path = require('path');
+const path = require("path");
 
-
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-    
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode:"development",
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      scriptloading:'defer',
-    }),
-],
-  module: {
-    rules: [
-      //css loader
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      //css image loader
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			scriptloading: "defer",
+		}),
+	],
+	module: {
+		rules: [
+			//css loader
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			//css image loader
 
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-// font loader
-     {
-       test: /\.(woff|woff2|eot|ttf|otf)$/i,
-       type: 'asset/resource',
-     },
-    ],
-    
-  },
-  
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			// font loader
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
 };
